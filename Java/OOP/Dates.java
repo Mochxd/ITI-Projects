226bb67419d832c94ab2e632b8fc0d3a36a6c31a
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-01-21 13:50:21 +0200

    Add files via upload

---
+++ Java/OOP/Dates.java
@@ -0,0 +1,14 @@
+package OOP;
+
+import java.util.Date;
+
+public class Dates {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+
+        long time = System.currentTimeMillis();
+        System.out.println(time);
+
+    }
+}
