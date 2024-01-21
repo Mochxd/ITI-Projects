e74e5ef33aff93174eed392e1b29b48ebc5c8bad
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-01-21 13:50:01 +0200

    Create Circle.java

---
+++ Java/OOP/Circle.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class Circle {
+    int rad;
+    boolean printed;
+    String name;
+    public Circle(){};
+
+    public Circle(int rad){
+        this.rad = rad;
+    };
+
+}
