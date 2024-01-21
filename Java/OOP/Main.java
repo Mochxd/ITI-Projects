19b45e6047b2e0b8e9db0639e43f070eb609cbc8
Mohamed Mostafa <122634626+Mochxd@users.noreply.github.com>
2024-01-21 13:51:05 +0200

    Add files via upload

---
+++ Java/OOP/Main.java
@@ -0,0 +1,12 @@
+package OOP;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Circle c2 = new Circle(5);
+
+        System.out.println(c1.rad);
+        System.out.println(c1.printed);
+        System.out.println(c1.name);
+    }
+}
